a9240fc8c97243aedf33c3bb4eafecb7b112356e
Ross Merrigan <email@rossmerrigan.com>
2017-09-18 22:15:19 +0200

    restored a missing test

--- query/cypher/queryPart_test.go
+++ query/cypher/queryPart_test.go
@@ -2,67 +2,68 @@ package cypher_test
 
 import (
 	"testing"
+
+	"github.com/RossMerr/Caudex.Graph/query"
+	"github.com/RossMerr/Caudex.Graph/query/cypher"
+	"github.com/RossMerr/Caudex.Graph/query/cypher/ast"
+	"github.com/RossMerr/Caudex.Graph/vertices"
 )
 
 func Test_ToQueryPath(t *testing.T) {
-	// edgePatn := &ast.EdgePatn{Body: &ast.EdgeBodyStmt{LengthMinimum: 2, LengthMaximum: 5}}
-	// vertexPatn := &ast.VertexPatn{Variable: "bar", Edge: edgePatn}
-	// var b bool
+	edgePatn := &ast.EdgePatn{Body: &ast.EdgeBodyStmt{LengthMinimum: 2, LengthMaximum: 5}}
+	vertexPatn := &ast.VertexPatn{Variable: "bar", Edge: edgePatn}
+	var b bool
 
-	// toPredicateVertex := func(*ast.VertexPatn) query.PredicateVertex {
-	// 	return func(v *vertices.Vertex) bool {
-	// 		return b
-	// 	}
-	// }
+	toPredicateVertex := func(*ast.VertexPatn) query.PredicateVertex {
+		return func(v *vertices.Vertex) bool {
+			return b
+		}
+	}
 
-	// toPredicateEdge := func(patn *ast.EdgePatn) query.PredicateEdge {
-	// 	return func(e *vertices.Edge) bool {
-	// 		return b
-	// 	}
-	// }
+	toPredicateEdge := func(patn *ast.EdgePatn) query.PredicateEdge {
+		return func(e *vertices.Edge) bool {
+			return b
+		}
+	}
 
-	// want := &cypher.Root{}
-	// vertexPath := &query.PredicateVertexPath{PredicateVertex: toPredicateVertex(vertexPatn)}
-	// vertexPath.SetNext(&query.PredicateEdgePath{PredicateEdge: toPredicateEdge(edgePatn)})
-	// want.SetNext(vertexPath)
+	want := &cypher.Root{}
+	vertexPath := &query.PredicateVertexPath{PredicateVertex: toPredicateVertex(vertexPatn)}
+	vertexPath.SetNext(&query.PredicateEdgePath{PredicateEdge: toPredicateEdge(edgePatn)})
+	want.SetNext(vertexPath)
 
-	// engine := cypher.NewEngine(NewFakeStorage())
-	// engine.ToPredicateEdge = toPredicateEdge
-	// engine.ToPredicateVertex = toPredicateVertex
+	parts, _ := cypher.NewParts().ToQueryPart(&ast.MatchStmt{Pattern: vertexPatn})
+	got := parts[0].Path
+	v, _ := got.Next().(query.VertexNext)
+	if v == nil {
+		t.Errorf("VertexNext")
+	}
 
-	// parts, _ := engine.ToQueryPart(&ast.MatchStmt{Pattern: vertexPatn})
-	// got := parts[0].Path
-	// v, _ := got.Next().(query.VertexNext)
-	// if v == nil {
-	// 	t.Errorf("VertexNext")
-	// }
+	pv, _ := v.(*query.PredicateVertexPath)
 
-	// pv, _ := v.(*query.PredicateVertexPath)
+	if pv == nil {
+		t.Errorf("PredicateVertexPath")
+	}
 
-	// if pv == nil {
-	// 	t.Errorf("PredicateVertexPath")
-	// }
+	e, _ := pv.Next().(query.EdgeNext)
+	if e == nil {
+		t.Errorf("EdgeNext")
+	}
 
-	// e, _ := pv.Next().(query.EdgeNext)
-	// if e == nil {
-	// 	t.Errorf("EdgeNext")
-	// }
+	pe, _ := e.(*query.PredicateEdgePath)
+	if pe == nil {
+		t.Errorf("PredicateEdgePath")
+	}
 
-	// pe, _ := e.(*query.PredicateEdgePath)
-	// if pe == nil {
-	// 	t.Errorf("PredicateEdgePath")
-	// }
+	if pe.Length().Minimum != 2 {
+		t.Errorf("Minimum")
+	}
 
-	// if pe.Length().Minimum != 2 {
-	// 	t.Errorf("Minimum")
-	// }
+	if pe.Length().Maximum != 5 {
+		t.Errorf("Maximum")
+	}
 
-	// if pe.Length().Maximum != 5 {
-	// 	t.Errorf("Maximum")
-	// }
-
-	// last, _ := pe.Next().(query.VertexNext)
-	// if last != nil {
-	// 	t.Errorf("VertexNext")
-	// }
+	last, _ := pe.Next().(query.VertexNext)
+	if last != nil {
+		t.Errorf("VertexNext")
+	}
 }
