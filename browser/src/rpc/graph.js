9056cba96dd10ce637f3815a484609dc60e98d19
Ross Merrigan <email@rossmerrigan.com>
2017-12-16 17:03:52 +0100

    moved folder around

--- browser/src/rpc/graph.js
+++ browser/src/rpc/graph.js
@@ -1,10 +1,10 @@
-import client from './vertex_pb';
+import rpc from './vertex_pb';
 
 export default function () { 
 
     var myHeaders = new Headers();
     myHeaders.append('Content-Type', 'application/grpc');
-    var request = new client.QueryRequest({});
+    var request = new rpc.QueryRequest({});
     request.setText("hi");  
 
     fetch('https://localhost:8080/client.Graph/Query', {method: 'POST', headers: myHeaders, body: request.serializeBinary()}).then(function(response) {
