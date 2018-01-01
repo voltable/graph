eacd3f92f1c43d4322e6822edc845ee419971b9c
Ross Merrigan <email@rossmerrigan.com>
2018-01-01 12:36:06 +0100

    fix canvas width and height

--- browser/src/components/D3Visualization/index.js
+++ browser/src/components/D3Visualization/index.js
@@ -1,9 +1,9 @@
-import {Element as PolymerElement} from "@polymer/polymer/polymer-element.js";
+import { Element as PolymerElement } from "@polymer/polymer/polymer-element.js";
 import "@polymer/font-roboto/roboto.js";
 import * as d3 from 'd3/build/d3'
 
 import css from './style.scss';
-import  template from './template.html';
+import template from './template.html';
 
 export default class D3Visualization extends PolymerElement {
 
@@ -15,63 +15,53 @@ export default class D3Visualization extends PolymerElement {
     ${template}`
   }
 
-  constructor() {
-    super();
-    this.name = '3.0 preview';
-  }
+  connectedCallback() {
+    super.connectedCallback();
 
-  ready() {
-    super.ready();
+    let canvas = this.$.canvas;
 
-    let canvas = this.$.canvas
-    let context = canvas.getContext('2d')
+    let width = canvas.clientWidth;
+    let height = canvas.clientHeight;
 
-    this.width = canvas.width
-    this.height = canvas.height
+    let context = canvas.getContext('2d');
+
+    canvas.width = width;
+    canvas.height = height;
 
     let color = d3.scaleOrdinal()
-        .range(d3.schemeCategory20);
+      .range(d3.schemeCategory20);
 
     let simulation = d3.forceSimulation()
-        .force("charge", d3.forceManyBody().strength(-18))
-        .force("link", d3.forceLink().iterations(4).id(function (d) { return d.id; }))
-        .force("x", d3.forceX())
-        .force("y", d3.forceY());
+      .force("charge", d3.forceManyBody().strength(-18))
+      .force("link", d3.forceLink().iterations(4).id(function (d) { return d.id; }))
+      .force("x", d3.forceX())
+      .force("y", d3.forceY());
 
     d3.json("/json/graph.json", (error, graph) => {
-        if (error) throw error;
+      if (error) throw error;
 
-        let users = d3.nest()
-            .key(function (d) { return d.user; })
-            .entries(graph.nodes)
-            .sort(function (a, b) { return b.values.length - a.values.length; });
+      let users = d3.nest()
+        .key(function (d) { return d.user; })
+        .entries(graph.nodes)
+        .sort(function (a, b) { return b.values.length - a.values.length; });
 
-        color.domain(users.map(function (d) { return d.key; }));
+      color.domain(users.map(function (d) { return d.key; }));
 
-        simulation
-            .nodes(graph.nodes)
-            .on("tick", () => {
-              this._ticked.bind(this)(context, graph, users, color);
-            });
+      simulation
+        .nodes(graph.nodes)
+        .on("tick", () => {
+          this._ticked.bind(this)(context, graph, users, color, width, height);
+        });
 
-        simulation.force("link")
-            .links(graph.links);
+      simulation.force("link")
+        .links(graph.links);
     });
   }
 
-  static get properties() {
-    width: {
-      Type: Number 
-    }
-    height: {
-      Type: Number 
-    }
-  }
-
-  _ticked(context, graph, users, color) {
-    context.clearRect(0, 0, this.width, this.height);
+  _ticked(context, graph, users, color, width, height) {
+    context.clearRect(0, 0, width, height);
     context.save();
-    context.translate(this.width / 2, this.height / 2);
+    context.translate(width / 2, height / 2);
 
     context.beginPath();
     graph.links.forEach((d) => this._drawLink(context, d));
@@ -79,10 +69,10 @@ export default class D3Visualization extends PolymerElement {
     context.stroke();
 
     users.forEach((user) => {
-        context.beginPath();
-        user.values.forEach((d) => this._drawNode(context, d));
-        context.fillStyle = color(user.key);
-        context.fill();
+      context.beginPath();
+      user.values.forEach((d) => this._drawNode(context, d));
+      context.fillStyle = color(user.key);
+      context.fill();
     });
 
     context.restore();
