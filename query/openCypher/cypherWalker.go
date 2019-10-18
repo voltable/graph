c8aa93c8159e052f847173f2a21ed6049e0e6ce9
Ross Merrigan <email@rossmerrigan.com>
2019-10-18 12:13:03 +0200

    added walker code for openCyhper

--- query/openCypher/cypherWalker.go
+++ query/openCypher/cypherWalker.go
@@ -1,1 +1,12 @@
 package openCypher
+
+import (
+	"github.com/antlr/antlr4/runtime/Go/antlr"
+	"github.com/voltable/graph/query/openCypher/parser"
+)
+
+type cypherWalker struct {
+	*parser.BaseCypherListener
+	errors []antlr.ErrorNode
+	stack  []interface{}
+}
